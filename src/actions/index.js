b5b058ea6451ec436060bf1fb9db1e269cf1fd41
OD-002 <od-002@Rachels-MBP.local>
2018-07-24 18:11:08 -0700

    added add_post, post_detail and post_index routes

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+
+export const FETCH_POSTS = 'fetch_posts';
+
+export function fetchPosts() {
+    return {
+        type: FETCH_POSTS
+    };
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/post_index';
+import AddPost from './components/add_post';
+import PostDetail from './components/post_detail';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex}/>
+        <Route path="/posts/new" component={AddPost}/>
+        <Route path="/posts/:id" component={PostDetail}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
